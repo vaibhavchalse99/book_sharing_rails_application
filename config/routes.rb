6bb6fa1063a5fe884f52db3c3addeabe756dc3da
vaibhav chalse <v-vaibhav.chalse@rebelfoods.com>
2023-02-04 17:08:35 +0530

    Devise basic configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :transactions
+  resources :books
+  resources :users
+  root to: "book#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
