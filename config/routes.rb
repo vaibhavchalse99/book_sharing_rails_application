c9cf982426b4b8ed39c91108d9e97ca555d80c83
vaibhav chalse <v-vaibhav.chalse@rebelfoods.com>
2023-02-04 17:11:01 +0530

    Add root route set to books index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :transactions
   resources :books
   resources :users
-  root to: "book#index"
+  root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
